2ca75a469bac7e1582209121b57ffe68919d1220
vutrn <121750583+vutrn@users.noreply.github.com>
2023-04-07 15:59:34 +0700

    modified:   public/css/app.css 	new file:   public/js/script.js 	# modified:   resources/views/auth/login.blade.php 	# modified:   resources/views/layouts/app.blade.php 	# modified:   resources/views/welcome.blade.php 	# modified:   routes/web.php 	# modified:   vendor/composer/autoload_classmap.php 	# modified:   vendor/composer/autoload_static.php 	# modified:   vendor/composer/installed.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('login', function () {
+    return view('login');
+});
 
+
+Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
