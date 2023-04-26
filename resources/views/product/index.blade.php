6b63a71acf724635b2e21214a01586e3b1508c4d
vutrn <121750583+vutrn@users.noreply.github.com>
2023-04-27 00:41:40 +0700

    deleted:    public/images/food/pesto-pasta.jpg 	deleted:    public/images/food/vegetable-salad.jpg 	new file:   public/images/interior/beige-padded-chair.jpg 	new file:   public/images/interior/beige-wooden-stool.jpg 	new file:   public/images/interior/ceiling-light-bulb.jpg 	new file:   public/images/interior/gray-padded-chair.jpg 	new file:   public/images/interior/lampshade.jpg 	new file:   public/images/interior/pendant-lamp.jpg 	new file:   public/images/interior/red-and-black-chair.jpg 	new file:   public/images/interior/round-table.jpg 	new file:   public/images/interior/round-white-3-pieces-set.jpg 	new file:   public/images/interior/sofa-chair.jpg 	new file:   public/images/interior/wooden-dining-table.jpg 	new file:   public/images/interior/yellow-padded-chair.jpg 	new file:   public/images/tools/bunch-of-wrenches.jpg 	new file:   public/images/tools/hammer.jpg 	new file:   public/images/tools/hex-bolt.jpg 	new file:   public/images/tools/pliers.jpg 	new file:   public/images/tools/ryobi-brushless-drill.jpg 	new file:   public/images/tools/shovel.jpg 	new file:   public/images/tools/wooden-hammer.jpg 	modified:   resources/views/cart/detail.blade.php 	modified:   resources/views/dashboard.blade.php 	modified:   resources/views/product/detail.blade.php 	modified:   resources/views/product/index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,27 +2,29 @@
 <x-app-layout>
   <section class="Show all product">
     <div class="container">
-      <h1 class="text-center text-4xl my-12 font-semibold">All Products</h1>
-      <div class="row"> 
-        <div id="add-to-cart-message"></div>  
+      <h1 class="my-12 text-center text-4xl font-semibold">All Products</h1>
+      <div class="row">
+        <div id="add-to-cart-message"></div>
         @foreach ($products as $product)
           <div class="col-2 pb-3">
-            <div class="card border-none transition-shadow hover:shadow-2xl">
-              <img src="{{ asset($product->image) }}" alt="Product Image" class="border-none">
-              <div class="product-info">
-              <p class="product-title line-clamp-2 text-ellipsis px-2 text-lg">{{ $product->name }}</p>
-              <p class="product-price pt-2 font-bold"> $ {{ $product->price }}</p>
+            <a href="{{ url('products/{id}') }}">
+              <div class="card w-full border-none transition-shadow hover:shadow-2xl">
+                <img src="{{ asset($product->image) }}" alt="Product Image" class="h-60 border-none object-cover">
+                <div class="product-info">
+                  <p class="product-title line-clamp-2 text-ellipsis h-12">{{ $product->name }}</p>
+                  <p class="product-price pt-2 font-bold"> $ {{ $product->price }}</p>
+                </div>
               </div>
-              <button data-id="{{ $product->id }}" class="add-to-cart bg-black py-1 text-white">Add to Cart</button>
-            </div>
+            </a>
+            <button data-id="{{ $product->id }}" class="add-to-cart bg-black py-1 text-white transition-opacity hover:opacity-70 w-full">Add to Cart</button>
           </div>
         @endforeach
       </div>
-                 @if ($products->links())
-                <div class="mt-4">
-                    {{ $products->links() }}
-                </div> 
-                @endif 
+      @if ($products->links())
+        <div class="mt-4">
+          {{ $products->links() }}
+        </div>
+      @endif
     </div>
   </section>
 </x-app-layout>
