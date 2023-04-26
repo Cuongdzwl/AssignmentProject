5d4872874eecca2580dd3441772fb1bf61c30f01
Cuongdzwl <xxxcuongxxx18@gmail.com>
2023-04-27 02:25:35 +0700

    modified:   resources/views/product/index.blade.php 	modified:   resources/views/search.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,12 +7,12 @@
         <div id="add-to-cart-message"></div>
         @foreach ($products as $product)
           <div class="col-2 pb-3">
-            <a href="{{ url('products/{id}') }}">
+            <a href="/products/{{ $product->id }}">
               <div class="card w-full border-none transition-shadow hover:shadow-2xl">
                 <img src="{{ asset($product->image) }}" alt="Product Image" class="h-60 border-none object-cover">
-                <div class="product-info">
-                  <p class="product-title line-clamp-2 text-ellipsis h-12">{{ $product->name }}</p>
-                  <p class="product-price pt-2 font-bold"> $ {{ $product->price }}</p>
+                <div class="product-info mt-1 px-3">
+                  <p class="product-title line-clamp-2 text-ellipsis h-12 font-bold mt-1">{{ $product->name }}</p>
+                  <p class="product-price pt-2 font-light mb-3"> $ {{ $product->price }}</p>
                 </div>
               </div>
             </a>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,33 +1,30 @@
 @section('title', 'Product Index')
 <x-app-layout>
-    <section class="Show all product">
-        <div class="container">
-            <a href="{{ route('product') }}">Back</a>
-
-            <div>{{ $keyword }}</div>
-            <div class="row">
-                <div id="add-to-cart-message"></div>
-                @foreach ($products as $product)
-                    <div class="col-md-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ asset($product->image) }}" alt="Product Image" class="img-fluid">
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                                <p class="card-text">{{ $product->description }}</p>
-                                <p class="card-text">Price: ${{ $product->price }}</p>
-                                <a href="{{ route('product.detail', $product->id) }}" data-id="{{ $product->id }}"
-                                    class="add-to-cart btn btn-primary">Add to
-                                    Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+  <section class="Show all product">
+    <div class="container">
+      <h1 class="my-12 text-center text-4xl font-semibold">Products for keyword: <span class="italic">{{$keyword}}...</span></h1>
+      <div class="row">
+        <div id="add-to-cart-message"></div>
+        @foreach ($products as $product)
+          <div class="col-2 pb-3">
+            <a href="/products/{{ $product->id }}">
+              <div class="card w-full border-none transition-shadow hover:shadow-2xl">
+                <img src="{{ asset($product->image) }}" alt="Product Image" class="h-60 border-none object-cover">
+                <div class="product-info mt-1 px-3">
+                  <p class="product-title line-clamp-2 text-ellipsis h-12 font-bold mt-1">{{ $product->name }}</p>
+                  <p class="product-price pt-2 font-light mb-3"> $ {{ $product->price }}</p>
+                </div>
+              </div>
+            </a>
+            <button data-id="{{ $product->id }}" class="add-to-cart bg-black py-1 text-white transition-opacity hover:opacity-70 w-full">Add to Cart</button>
+          </div>
+        @endforeach
+      </div>
+      @if ($products->links())
+        <div class="mt-4">
+          {{ $products->links() }}
         </div>
-        @if ($products->links())
-            <div class="mt-4">
-                {{ $products->links() }}
-            </div>
-        @endif
-    </section>
+      @endif
+    </div>
+  </section>
 </x-app-layout>
