9b9d6699d56baa235c079fc63baee701ec16eb0d
vutrn <121750583+vutrn@users.noreply.github.com>
2023-04-26 17:26:51 +0700

    modified:   public/css/layout.css 	new file:   public/images/accessory/black-tote-bag.jpg 	new file:   public/images/accessory/brown-framed-eyeglasses.jpg 	new file:   public/images/accessory/brown-leather-handbag.jpg 	new file:   public/images/accessory/gold-black-analog-watch.jpg 	new file:   public/images/accessory/golden-star-earrings.jpg 	new file:   public/images/accessory/red-leather-handbag.jpg 	new file:   public/images/accessory/silver-framed-eyeglasses.jpg 	new file:   public/images/accessory/silver-gemstone-pendant-necklace.jpg 	new file:   public/images/food/baked-pancakes.jpg 	new file:   public/images/food/blueberry-toast-bread.jpg 	new file:   public/images/food/burger.jpg 	new file:   public/images/food/cooked-salmon.jpg 	new file:   public/images/food/pesto-pasta.jpg 	new file:   public/images/food/pizza.jpg 	new file:   public/images/food/raspberry-cake.jpg 	new file:   public/images/food/sandwich-with-boiled-egg.jpg 	new file:   public/images/food/strawberry-yogurt.jpg 	new file:   public/images/food/tomahawk-steak.jpg 	new file:   public/images/food/vegetable-salad.jpg 	modified:   resources/css/home.css 	deleted:    resources/css/layout.css 	modified:   resources/css/login.css 	deleted:    resources/css/product-detail.css 	modified:   resources/js/data/loadProducts.js 	modified:   resources/js/home.js 	modified:   resources/views/cart/detail.blade.php 	modified:   resources/views/dashboard.blade.php 	modified:   resources/views/product/detail.blade.php 	modified:   resources/views/product/index.blade.php

--- resources/views/cart/detail.blade.php
+++ resources/views/cart/detail.blade.php
@@ -25,11 +25,11 @@
                     <td>{{ $item->name }}</td>
                     <td>{{ $item->price }}</td>
                     <td>
-                      <input type="number" value="{{ $item->quantity }}" min="1" class="form-control">
+                      <input  type="number" value="{{ $item->quantity }}" min="1" class="form-control quantity">
                     </td>
                     <td id="subtotal">{{ $item->price * $item->quantity }}</td>
-                    <td>
-                      <a href="#" class="btn btn-danger">Delete</a>
+                    <td>            
+                      <button class="btn btn-danger delete" data-id="{{ $item->product_id }}">Delete</button>
                     </td>
                   </tr>
                 @endforeach
@@ -38,20 +38,21 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4">
-        <div class="totals-item">
-          <label>Items:</label>
-          <div class="totals-value" id="cart-subtotal">0</div>
+      <div class="col-sm-4">
+        <div class="totals-item flex justify-between pb-3">
+          <label>Items: </label>
+          <div class="totals-value" id="cart-subtotal">$0</div>
         </div>
-        <div class="totals-item">
+        <div class="totals-item flex justify-between pb-3">
           <label>Tax (5%):</label>
-          <div class="totals-value" id="cart-tax">0</div>
+          <div class="totals-value" id="cart-tax">$0</div>
         </div>
-        <div class="totals-item totals-item-total">
+        <hr>
+        <div class="totals-item flex justify-between py-3 font-bold text-red-500">
           <label>Total:</label>
-          <div class="totals-value" id="cart-total">0</div>
+          <div class="totals-value" id="cart-total">$0</div>
         </div>
-        <button class="checkout">Checkout</button>
+        <button class="checkout bg-dark w-full p-2 text-white transition-opacity hover:opacity-70">Checkout</button>
       </div>
 
     </div>
